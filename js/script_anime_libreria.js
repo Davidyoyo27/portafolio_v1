a1aec1430ce15a16f72047f38090d5da30edca7c
Davidyoyo27 <davidca.vega@gmail.com>
2025-03-27 23:59:05 -0300

    uptdated information of experience, solve css problems and added improvements to animation in the start

--- js/script_anime_libreria.js
+++ js/script_anime_libreria.js
@@ -1,50 +1,50 @@
-function animationMoveLetter() {
-  // bloque de codigo que ejecuta los parametros de la libreria
-  let tl = anime.timeline({
-    targets: ".ini_text p",
-    delay: function (el, i) {
-      return i * 200;
-    },
-    duration: 500, // Can be inherited
-    easing: "easeOutExpo", // Can be inherited
-    direction: "alternate", // Is not inherited
-    loop: 1, // Is not inherited
-  });
 
-  tl.add({
-    translateX: 120,
-    // override the easing parameter
-    easing: "spring",
-  })
-    .add({
-      opacity: 0.5,
-      scale: 2,
-    })
-    .add({
-      // override the targets parameter
-      targets: ".ini_text p",
-      rotate: 180,
-    })
-    .add({
-      translateX: 0,
-      scale: 1,
-    });
+const button_play = document.getElementById("btn_play");
+const button_pause = document.getElementById("btn_pause");
+
+// true: permite que la animacion se detenga cuando se cambia de pestaña
+anime.suspendWhenDocumentHidden = true; // default true
+
+// bloque de codigo que ejecuta los parametros de la libreria
+let animationData = anime.timeline({
+  targets: ".ini_text p",
+  delay: function (el, i) {
+    return i * 100;
+  },
+  duration: 250, // Can be inherited  500
+  easing: "easeOutExpo", // Can be inherited
+  direction: "alternate", // Is not inherited
+  autoplay: false,
+  complete: () => {
+    // mostramos el boton que inicia la animacion cuando esta concluye
+    button_play.style.display = "block"
+  }
+});
+
+animationData.add({
+  translateX: 120,
+  // override the easing parameter
+  easing: "spring",
+})
+.add({
+  opacity: 0.5,
+  scale: 2,
+})
+.add({
+  // override the targets parameter
+  targets: ".ini_text p",
+  rotate: 180,
+})
+.add({
+  translateX: 0,
+  scale: 1,
+});
+
+function playAnimation(){
+  button_play.style.display = "none";
+  return animationData.play();
 }
 
-function ejecutarAnimacionYEsperar() {
-  setTimeout(mostrarElemento, 72000);
-}
-
-function animacion() {
-  ocultarElemento();
-  animationMoveLetter();
-  ejecutarAnimacionYEsperar();
-}
-
-function ocultarElemento() {
-  document.getElementById("icon-refresh").style.display = "none";
-}
-
-function mostrarElemento() {
-  document.getElementById("icon-refresh").style.display = "block";
+function pauseAnimation(){
+  return animationData.pause();
 }
